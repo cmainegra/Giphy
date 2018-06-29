2130a55f73e599ec2e8ca91d89566e59c122a802
Charlie <cmainegra@protonmail.com>
2018-06-29 00:20:59 -0400

    somethingelse

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,8 @@
-$(document).on(ready {})
+$(document).ready(function()  {
 
 $("button").on("click", function() {
     // Grabbing and storing the data-animal property value from the button
-    var animal = $(this).attr("data-animal").val().trim;
+    var animal = $("#data-animal").val();
 
     // Constructing a queryURL using the animal name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -15,28 +15,28 @@ $("button").on("click", function() {
     })
       // After data comes back from the request
       .then(function(response) {
-        console.log(queryURL);
+        // console.log(queryURL);
 
         console.log(response);
         // storing the data from the AJAX request in the results variable
         var results = response.data;
 
         // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
+        for (var i = 0; i < 10; i++) {
 
           // Creating and storing a div tag
-          var animalDiv = $("<div>");
+          var animalDiv = $(".column");
 
           // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
+          // var p = $("<p>").text("Rating: " + results[i].rating);
 
           // Creating and storing an image tag
-          var animalImage = $("<img>");
+          var animalImage = $(".images");
           // Setting the src attribute of the image to a property pulled off the result item
           animalImage.attr("src", results[i].images.fixed_height.url);
 
           // Appending the paragraph and image tag to the animalDiv
-          animalDiv.append(p);
+          // animalDiv.append(p);
           animalDiv.append(animalImage);
 
           // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
@@ -44,3 +44,4 @@ $("button").on("click", function() {
         }
       });
   });
+});
