58ffd7e09fd801049e304b725ac129e90cce0aae
Charlie <cmainegra@protonmail.com>
2018-06-29 16:32:53 -0400

    morefunction

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,47 +1,22 @@
 $(document).ready(function()  {
-
 $("button").on("click", function() {
-    // Grabbing and storing the data-animal property value from the button
-    var animal = $("#data-animal").val();
-
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=iZUd4TnqSidctRLrT3nT3kTTfDoYFfoL";
-
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // After data comes back from the request
-      .then(function(response) {
-        // console.log(queryURL);
-
-        console.log(response);
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
-
-        // Looping through each result item
-        for (var i = 0; i < 10; i++) {
-
-          // Creating and storing a div tag
-          var animalDiv = $(".column");
-
-          // Creating a paragraph tag with the result item's rating
-          // var p = $("<p>").text("Rating: " + results[i].rating);
-
-          // Creating and storing an image tag
-          var animalImage = $(".images");
-          // Setting the src attribute of the image to a property pulled off the result item
-          animalImage.attr("src", results[i].images.fixed_height.url);
-
-          // Appending the paragraph and image tag to the animalDiv
-          // animalDiv.append(p);
-          animalDiv.append(animalImage);
-
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          $("#gifs-appear-here").prepend(animalDiv);
-        }
-      });
-  });
+var animal = $("#data-animal").val();
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+animal + "&api_key=iZUd4TnqSidctRLrT3nT3kTTfDoYFfoL";
+$.ajax({
+url: queryURL,
+method: "GET"
+})
+.then(function(response) {
+console.log(response);
+var results = response.data;
+for (var i = 0; i < 10; i++) {
+var animalDiv = $(".column");
+var animalImage = $("#images");
+animalImage.attr("src", results[i].images.fixed_height.url);
+animalDiv.append(animalImage);
+$("#gifs-appear-here").prepend(animalDiv);
+}
+});
+});
 });
