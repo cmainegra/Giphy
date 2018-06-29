1baaecf9ed4ac107367fccd5c6fca3b1f40a8244
Charlie <cmainegra@protonmail.com>
2018-06-29 16:39:05 -0400

    formatting

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,22 @@
-$(document).ready(function()  {
-$("button").on("click", function() {
-var animal = $("#data-animal").val();
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-animal + "&api_key=iZUd4TnqSidctRLrT3nT3kTTfDoYFfoL";
-$.ajax({
-url: queryURL,
-method: "GET"
-})
-.then(function(response) {
-console.log(response);
-var results = response.data;
-for (var i = 0; i < 10; i++) {
-var animalDiv = $(".column");
-var animalImage = $("#images");
-animalImage.attr("src", results[i].images.fixed_height.url);
-animalDiv.append(animalImage);
-$("#gifs-appear-here").prepend(animalDiv);
-}
-});
-});
+$(document).ready(function () {
+  $("button").on("click", function () {
+    var animal = $("#data-animal").val();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "&api_key=iZUd4TnqSidctRLrT3nT3kTTfDoYFfoL";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function (response) {
+        console.log(response);
+        var results = response.data;
+        for (var i = 0; i < 10; i++) {
+          var animalDiv = $(".column");
+          var animalImage = $("#images");
+          animalImage.attr("src", results[i].images.fixed_height.url);
+          animalDiv.append(animalImage);
+          $(".gifs-appear-here").prepend(animalDiv);
+        }
+      });
+  });
 });
